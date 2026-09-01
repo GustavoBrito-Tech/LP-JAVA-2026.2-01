7e54c571123c563623e05bd6e64ba43d6376d517
gustavo.brito <gustavo.santoss.102050@gmail.com>
2026-09-01 14:49:10 -0300

    Print no java

--- src/main/java/Opa.java
+++ src/main/java/Opa.java
@@ -1,6 +1,10 @@
 public class Opa {
     public static void main (String []args){
         System.out.println("Finalmente oi!");
+
+        String nome = "Gustavo Daniel";
+        System.out.println(nome);
+
     }
 
 }
