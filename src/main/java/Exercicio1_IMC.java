66884814ba1898e9aa7ac0d01906b0c5ba5d9a34
Gustavo Daniel <gustavo,santoss.102050@gmail.com>
2026-09-09 14:55:32 -0300

    operadores lógicos

---
+++ src/main/java/Exercicio1_IMC.java
@@ -0,0 +1,17 @@
+import javax.swing.JOptionPane;
+
+public class Exercicio1_IMC {
+    public static void main (String [] args) {
+        String nome = JOptionPane.showInputDialog("Olá! Qual seu nome? ");
+        String pesoStr = JOptionPane.showInputDialog("Digite o seu peso em KG: ");
+        Double peso = Double.parseDouble(pesoStr) ;
+
+        String alturaStr = JOptionPane.showInputDialog("Digite o sua altura em Metros: ");
+        Double altura = Double.parseDouble(alturaStr);
+
+        Double imc = peso * (altura * altura);
+
+        JOptionPane.showMessageDialog (null,"Olá " + nome + "! Este é o valor do seu IMC " + imc);
+
+    }
+}
